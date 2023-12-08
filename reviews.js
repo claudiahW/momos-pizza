bcaae27d7674420ca1fc9599652e484af1365349
claudiahW <murungiclaud@gmail.com>
2023-12-08 23:31:37 +0300

    link reviews page to home page

--- reviews.js
+++ reviews.js
@@ -1,37 +1,6 @@
-console.log('Hello')
-function getSize() {
-    var size = document.getElementById("pizzaSize").value;
-    
-    return parseInt(size);
-  }
-  function getCrust() {
-    var crust = document.getElementById("pizzaCrust").value;
-    return parseInt(crust);
-  }
-  
-  function getTopping() {
-    var topping = document.getElementById("pizzaTopping").value;
-    return parseInt(topping);
-  }
-  function getQuantity() {
-    var number = document.getElementById("quantity").value;
-    return parseInt(number);
-  }
-  console.log(getSize()) ;
-  function totalAmount() {
-      var totals = (getSize() + getCrust() + getTopping()) * getQuantity();
-      alert(
-      "Your order of "  +
-       getQuantity ("") +
-      " Pizza, " + 
-      " " +  "total to Shillings " + 
-       totals +
-      "" + ""+ ".Thank you for shopping with us!See you soon.")
-  };
-  
-  //local reviews data
+//local reviews data
 
-  const reviews = [
+const reviews = [
     {
       id: 1,
       name: "Susan Smith",
